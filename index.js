38f18eb37cf3a9f2782733058e665e8cac72eebc
Aslam45235 <101480522+Aslam45235@users.noreply.github.com>
2023-03-28 05:18:55 +0500

    updated

--- index.js
+++ index.js
@@ -3,13 +3,11 @@ import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from "cors";
 import path from "path";
-import dotenv from 'dotenv'
+
 
 import postRoutes from "./routes/posts.js";
 import userRoutes from "./routes/user.js";
 const app = express();
-dotenv.config();
-
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
@@ -18,7 +16,8 @@ app.use("/posts", postRoutes);
 app.use("/users", userRoutes);
 // db connection
 
-
+const url =
+  "mongodb+srv://markhor:Markhor45235@cluster0.slcaj.mongodb.net/?retryWrites=true&w=majority/test";
 
 const PORT = process.env.PORT || 5000;
 
